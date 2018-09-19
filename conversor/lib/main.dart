6ed2f161613b73c2e438d7605c4c8ab52eddd6f5
Wagner Nogueira <wagner@ephrom.com.br>
2018-09-19 14:10:05 -0300

    Criando tela com scroll e campos

--- conversor/lib/main.dart
+++ conversor/lib/main.dart
@@ -9,8 +9,8 @@ const req =
 
 void main() async {
   runApp(MaterialApp(
-    home: Home()
-  ));
+      home: Home(),
+      theme: ThemeData(hintColor: Colors.amber, primaryColor: Colors.white)));
 }
 
 Future<Map> getData() async {
@@ -18,25 +18,86 @@ Future<Map> getData() async {
   return json.decode(res.body);
 }
 
-class Home extends StatefulWidget{
+class Home extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => _State();
 }
 
-class _State extends State<Home>{
+class _State extends State<Home> {
+  double dolar;
+  double euro;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
-        title: Text("Conversor"),
-        backgroundColor: Colors.amber,
-        centerTitle: true,
+          title: Text("Conversor"),
+          backgroundColor: Colors.amber,
+          centerTitle: true),
+      body: FutureBuilder<Map>(
+          future: getData(),
+          builder: (context, snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+              case ConnectionState.waiting:
+                return Center(
+                  child: Text("Carregando dados...",
+                      style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                      textAlign: TextAlign.center),
+                );
+              default:
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text("Ocorreu um erro ao carregar os dados",
+                        style: TextStyle(color: Colors.amber, fontSize: 25.0),
+                        textAlign: TextAlign.center),
+                  );
+                } else {
+                  dolar = snapshot.data["results"]["currencies"]["USD"]["buy"];
+                  euro = snapshot.data["results"]["currencies"]["EUR"]["buy"];
 
-      ),
-
+                  return SingleChildScrollView(
+                      padding: EdgeInsets.all(10.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          Icon(Icons.monetization_on,
+                              size: 150.0, color: Colors.amber[300]),
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Reais",
+                                labelStyle: TextStyle(color: Colors.amber),
+                                border: OutlineInputBorder(),
+                                prefixText: "R\$"),
+                            style:
+                                TextStyle(color: Colors.amber, fontSize: 25.0),
+                          ),
+                          Divider(),
+                          TextField(
+                            decoration: InputDecoration(
+                                labelText: "Dólares",
+                                labelStyle: TextStyle(color: Colors.amber),
+                                border: OutlineInputBorder(),
+                                prefixText: "US\$"),
+                            style:
+                                TextStyle(color: Colors.amber, fontSize: 25.0),
+                          ),
+                          Divider(),
+                           TextField(
+                            decoration: InputDecoration(
+                                labelText: "Euros",
+                                labelStyle: TextStyle(color: Colors.amber),
+                                border: OutlineInputBorder(),
+                                prefixText: "€\$"),
+                            style:
+                                TextStyle(color: Colors.amber, fontSize: 25.0),
+                          )
+                        ],
+                      ));
+                }
+            }
+          }),
     );
   }
 }
-
